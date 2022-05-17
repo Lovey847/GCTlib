ca77e115b418b6a8e1befab0b8442ff399b7f77a
Lovey847 <lovey847@gmail.com>
2022-05-17 15:40:12 -0500

    Convert decoding sample to C++

--- sample/src/decode.cpp
+++ sample/src/decode.cpp
@@ -12,36 +12,31 @@
 
 #include "gct/gctlib.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stddef.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cstddef>
 
-/* Function prototypes */
+// Function prototypes
 static void *GetGCTFile(int argc, char **argv);
 static void WriteFile(int argc, char **argv, void *imageData, gct_iptr dataSize);
 
 int main(int argc, char **argv) {
-  gct_color_t *imageData;
-  void *gctFile;
-  gct_error_t err;
-  gct_iptr dataSize;
-  int width, height;
+  void *gctFile = GetGCTFile(argc, argv);
 
-  gctFile = GetGCTFile(argc, argv);
-
-  /* Get decoded image data size */
-  dataSize = gct_DecodedSize(gctFile);
+  // Get decoded image data size
+  gct_iptr dataSize = gct_DecodedSize(gctFile);
   if (dataSize < 0) {
     printf("ERROR: Cannot get decoded image data size! (%s)\n", gct_StrError(dataSize));
     free(gctFile);
     return 1;
   }
 
-  imageData = malloc(dataSize);
+  gct_color_t *imageData = (gct_color_t*)malloc(dataSize);
 
-  /* Decode GCT file into imageData */
-  err = gct_Decode(gctFile, &width, &height, imageData);
+  // Decode GCT file into imageData
+  int width, height;
+  gct_error_t err = gct_Decode(gctFile, &width, &height, imageData);
   if (err != gct_SUCCESS) {
     printf("ERROR: Cannot decode GCT image data! (%s)\n", gct_StrError(err));
     free(gctFile);
@@ -53,50 +48,44 @@ int main(int argc, char **argv) {
 
   printf("Image size: %d %d\n", width, height);
 
-  /* Free resources and exit */
+  // Free resources and exit
   free(gctFile);
   free(imageData);
 
   return 0;
 }
 
-/* Arguments slots */
+// Arguments slots
 #define ARG_INPUT 1
 #define ARG_OUTPUT 2
 
-/* Open GCT file, and return file data */
+// Open GCT file, and return file data
 static void *GetGCTFile(int argc, char **argv) {
   const char *inputName;
-  FILE *f;
-  void *ret;
-  size_t retSize;
-
   if (argc < 2) inputName = "sampleImage.gct";
   else inputName = argv[ARG_INPUT];
 
-  f = fopen(inputName, "rb");
+  FILE *f = fopen(inputName, "rb");
 
-  /* Get file size */
+  // Get file size
   fseek(f, 0, SEEK_END);
-  retSize = ftell(f);
+  const size_t retSize = ftell(f);
   fseek(f, 0, SEEK_SET);
 
-  ret = malloc(retSize);
+  void *ret = malloc(retSize);
   fread(ret, 1, retSize, f);
   fclose(f);
 
   return ret;
 }
 
-/* Write raw data to file */
+// Write raw data to file
 static void WriteFile(int argc, char **argv, void *imageData, gct_iptr dataSize) {
   const char *outputName;
-  FILE *f;
-
   if (argc < 3) outputName = "sampleImage.data";
   else outputName = argv[ARG_OUTPUT];
 
-  f = fopen(outputName, "wb");
+  FILE *f = fopen(outputName, "wb");
   fwrite(imageData, 1, dataSize, f);
   fclose(f);
 }
